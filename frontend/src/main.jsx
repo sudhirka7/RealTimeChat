fcca400de4ab40260c351c061d28719a399bfb7e
Sudhir kumar <121374488+sudhirka7@users.noreply.github.com>
2025-05-15 12:36:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://gp-chat.onrender.com"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
